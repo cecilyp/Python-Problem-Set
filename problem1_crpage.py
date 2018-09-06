ec731f3ad5bb7081466a4e57f2ea60ff2ca820b6
cecilyp <cecilyrbpage@gmail.com>
2018-09-06 17:36:24 -0400

    added files

---
+++ problem1_crpage.py
@@ -0,0 +1,20 @@
+# STAT/CS 287
+# HW 01
+#
+# Name: <FILL ME IN>
+# Date: <FILL ME IN>
+
+
+def similarity(A,B):
+    '''
+
+    :param A:
+    :param B:
+    :return:
+    '''
+    ### YOUR CODE HERE ###
+
+### YOUR CODE HERE ###
+
+
+
---
+++ problem2_crpage.py
@@ -0,0 +1,17 @@
+# STAT/CS 287
+# HW 01
+#
+# Name: <Cecily Page>
+# Date: <Thursday, September 6th 2018>
+
+import random
+
+
+def coin_flip(p):
+    """
+
+    :param p:
+    :return:
+    """
+
+### YOUR CODE HERE ###
